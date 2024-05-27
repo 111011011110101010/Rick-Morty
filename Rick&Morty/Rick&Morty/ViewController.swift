a7e5aebbe33704cee76f4675b01f2570fe232fbc
Vladislav <vlad_zaharchenko@list.ru>
2024-05-27 23:36:37 +0300

    init project

--- Rick&Morty/Rick&Morty/ViewController.swift
+++ Rick&Morty/Rick&Morty/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  Rick&Morty
-//
-//  Created by newuser on 27.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
     }
-
-
 }
 
